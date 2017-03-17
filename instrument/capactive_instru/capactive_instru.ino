4597e342b511e9e4021dce5a9f3bf02656bc7613
Hariharan <theroboticlabs@gmail.com>
2017-03-17 17:45:19 +0530

    Support for capacitive keys

--- instrument/capactive_instru/capactive_instru.ino
+++ instrument/capactive_instru/capactive_instru.ino
@@ -1,43 +1,22 @@
-#define portOfPin(P)\
-  (((P)>=0&&(P)<8)?&PORTD:(((P)>7&&(P)<14)?&PORTB:&PORTC))
-#define ddrOfPin(P)\
-  (((P)>=0&&(P)<8)?&DDRD:(((P)>7&&(P)<14)?&DDRB:&DDRC))
-#define pinOfPin(P)\
-  (((P)>=0&&(P)<8)?&PIND:(((P)>7&&(P)<14)?&PINB:&PINC))
-#define pinIndex(P)((uint8_t)(P>13?P-14:P&7))
-#define pinMask(P)((uint8_t)(1<<pinIndex(P)))
+#include <ADCTouch.h>
 
-#define pinAsInput(P) *(ddrOfPin(P))&=~pinMask(P)
-#define pinAsInputPullUp(P) *(ddrOfPin(P))&=~pinMask(P);digitalHigh(P)
-#define pinAsOutput(P) *(ddrOfPin(P))|=pinMask(P)
-#define digitalLow(P) *(portOfPin(P))&=~pinMask(P)
-#define digitalHigh(P) *(portOfPin(P))|=pinMask(P)
-#define isHigh(P)((*(pinOfPin(P))& pinMask(P))>0)
-#define isLow(P)((*(pinOfPin(P))& pinMask(P))==0)
-#define digitalState(P)((uint8_t)isHigh(P))
-const int swi1 = 11;
-const int swi2 = 10;
-const int swi3 = 9;
-const int swi4 = 8;
-const int swi5 = 7;
-const int swi6 = 6;
+
+const int swi1 = A0;
+const int swi2 = A1;
+const int swi3 = A2;
+const int swi4 = A3;
+const int swi5 = A4;
+const int swi6 = A5;
 const int swi7 = 5;
 const int swi8 = 4;
 const int sel = 3; //To select mode: ON-sequencer or OFF-normal instrument
 int red;
 int change;
+int threshold = 40;
+int s[8];
 void setup() {
-  // put your setup code here, to run once:
-   pinAsInput(swi1);
-  pinAsInput(swi2);
-  pinAsInput(swi3);
-   pinAsInput(swi4);
-    pinAsInput(swi5);
-     pinAsInput(swi6);
-      pinAsInput(swi7);
-       pinAsInput(swi8);
     Serial.begin(115200);
-  if(digitalRead(sel)==HIGH)
+  if(digitalRead(sel)==1)
 {
  Serial.println("S1");
 }
@@ -47,66 +26,83 @@ else
 }
 
 red = analogRead(A0); //Rate adjustment
+s[0] = ADCTouch.read(swi1,500);
+s[1] = ADCTouch.read(swi2,500);
+s[2] = ADCTouch.read(swi3,500);
+s[3] = ADCTouch.read(swi4,500);
+s[4] = ADCTouch.read(swi5,500);
+s[5] = ADCTouch.read(swi6,500);
+s[6] = ADCTouch.read(swi7,500);
+s[7] = ADCTouch.read(swi8,500);
 }
 void loop() {
-change = analogRead(A0); //Rate adjustment
+/*change = analogRead(A0); //Rate adjustment
 if(abs(red-change)>80)
 {
   red=change;
 Serial.print("R,");
 Serial.println(red);
 //Serial.println(change);
-}
-if(digitalState(swi1) == HIGH)
+}*/
+if(capRead(swi1,1) == 1)
 {
- while(digitalState(swi1) == HIGH);
+ while(capRead(swi1,1) == 1);
  Serial.println("L1");
  Serial.flush();
 }
 
-if(digitalState(swi2) == HIGH)
+if(capRead(swi2,2) == 1)
 {
-  while(digitalState(swi2) == HIGH);
+  while(capRead(swi2,2) == 1);
  Serial.println("L2");
  Serial.flush();
 }
 
-if(digitalState(swi3) == HIGH)
+if(capRead(swi3,3) == 1)
 {
- while(digitalState(swi3) == HIGH);
+ while(capRead(swi3,3) == 1);
  Serial.println("L3");
  Serial.flush();
 }
 
-if(digitalState(swi4) == HIGH)
+if(capRead(swi4,4) == 1)
 {
-  while(digitalState(swi4) == HIGH);
+  while(capRead(swi4,4) == 1);
  Serial.println("L4");
  Serial.flush();
 }
-if(digitalState(swi5) == HIGH)
+if(capRead(swi5,5) == 1)
 {
-  while(digitalState(swi5) == HIGH);
+  while(capRead(swi5,5) == 1);
  Serial.println("L5");
  Serial.flush();
 }
-if(digitalState(swi6) == HIGH)
+if(capRead(swi6,6) == 1)
 {
-  while(digitalState(swi6) == HIGH);
+  while(capRead(swi6,6) == 1);
  Serial.println("L6");
  Serial.flush();
 }
-if(digitalState(swi7) == HIGH)
+if(capRead(swi7,7) == 1)
 {
-  while(digitalState(swi7) == HIGH);
+  while(capRead(swi7,7) == 1);
  Serial.println("L7");
  Serial.flush();
 }
-if(digitalState(swi8) == HIGH)
+if(capRead(swi8,8) == 1)
 {
- while(digitalState(swi8) == HIGH);
+ while(capRead(swi8,8) == 1);
  Serial.println("L8");
  Serial.flush();
 }
 delay(30);
 }
+
+int capRead(int swi,int p)
+{
+  int c = ADCTouch.read(swi);
+  c -=s[p-1];
+  return (c > threshold);
+}
+
+
